4beaaf4804bbcd4231c53feb4f39d1025dd9d03c
IanaVatagina <vatagina.iana@gmail.com>
2024-04-07 16:12:07 +0300

    B202

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+	T data[size];
+	int top;
+public:
+	TStack() :top(-1) {}
+	T get() const {
+		if (!isEmpty())
+			return data[top];
+		else
+			throw std::string("Empty");
+	}
+	bool isEmpty() const {
+		return top == 0;
+	}
+	bool isFull() const {
+		return top == size - 1;
+	}
+	T pop() {
+		if (top >= 0)
+			return data[top--];
+		else
+			throw std::string("End");
+	}
+	void push(T item) {
+		if (top <= size - 1)
+			data[++top] = item;
+		else
+			throw std::string("Full");
+	}
 };
 
 #endif  // INCLUDE_TSTACK_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -2,10 +2,63 @@
 #include <string>
 #include <map>
 #include "tstack.h"
+int priorityOperations(char operSymb) {
+	switch (operSymb)
+	{
+	case '(':
+		return 0;
+	case ')':
+		return 1;
+	case '+':
+		return 2;
+	case '-':
+		return 2;
+	case '*':
+		return 3;
+	case '/':
+		return 3;
+	default:
+		break;
+	}
+}
+
+bool isOperation(char nowSymb) {
+	return (nowSymb == '+' || nowSymb == '-' || nowSymb == '*' || nowSymb == '/');
+}
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+	std::string pstfxString;
+	TStack<char, 100> stack1;
+	for (int i = 0; i < inf.size(); i++) {
+		char currentElement = inf[i];
+		if (currentElement >= '0' && currentElement <= '9') {
+			pstfxString.push_back(currentElement);
+			pstfxString.push_back(' ');
+		} else if (isOperation(currentElement)) {
+			if (currentElement == '(')
+				stack1.push(currentElement);
+			else if ((!stack1.isEmpty()) && (priorityOperations(currentElement) > priorityOperations(stack1.get())))
+				stack1.push(currentElement);
+			else if ((!stack1.isEmpty()) && (priorityOperations(currentElement) <= priorityOperations(stack1.get()))) {
+				while ((!stack1.isEmpty()) && (priorityOperations(currentElement) > priorityOperations(stack1.get()))) {
+					pstfxString.push_back(stack1.pop());
+					pstfxString.push_back(' ');
+				}
+				stack1.push(currentElement);
+			} else if (stack1.isEmpty())
+				stack1.push(currentElement);
+		} else if (currentElement == ')') {
+			while ((!stack1.isEmpty()) && (stack1.get() != '(')) {
+				pstfxString.push_back(stack1.pop());
+				pstfxString.push_back(' ');
+			}
+		}
+	}
+	while (!stack1.isEmpty()) {
+		pstfxString.push_back(stack1.pop());
+		pstfxString.push_back(' ');
+	}
+    return pstfxString;
 }
 
 int eval(std::string pref) {
